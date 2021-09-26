56f079cab018c9733b9532609aa079c6fd47f849
jahid <info.jahidulislamjahid@gmail.com>
2021-09-26 23:34:44 +0600

    final polishing

--- src/components/PrimeMinister/PrimeMinister.js
+++ src/components/PrimeMinister/PrimeMinister.js
@@ -3,7 +3,7 @@ import './PrimeMinister.css';
 
 const PrimeMinister = (props) => {
     // console.log(props.primeMinister)
-    const {image,name,age,gender,headOfGovt,country,Salary} =props.primeMinister
+    const { image, name, age, gender, headOfGovt, country, Salary } = props.primeMinister
     return (
         <div className="col-md-4 p-2 text-center">
             <div className="card h-100 ms-auto card-border">
@@ -14,14 +14,14 @@ const PrimeMinister = (props) => {
                     <h6>Name : {name}</h6>
                     <p>Age : {age}</p>
                     <p>Gender : {gender}</p>
-                    <h6>Head of the Govt. : {headOfGovt}</h6>
+                    <h6>Head of : {headOfGovt}</h6>
                     <h6>Country : {country}</h6>
                     <h6>Salary : <small>${Salary}</small></h6>
                 </div>
                 <div className=" border-0 mb-2">
-                    <button onClick={()=>props.handleInvitation(props.primeMinister)}><i class="fas fa-paper-plane"></i> Send invitation</button>
-                </div>    
-            </div>        
+                    <button onClick={() => props.handleInvitation(props.primeMinister)}><i class="fas fa-paper-plane"></i> Send invitation</button>
+                </div>
+            </div>
         </div>
     );
 };
