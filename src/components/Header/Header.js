70e404c07f593039efdca0a1996dd9df39f71e18
jahid <info.jahidulislamjahid@gmail.com>
2021-09-26 15:36:36 +0600

    ui changes"

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const header = () => {
+    return (
+        <div className="header-items">
+            <h2>Top One Hundred Artits Around The World</h2>
+        </div>
+    );
+};
+
+export default header;
